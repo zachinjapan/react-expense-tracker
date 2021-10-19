d7bc487dd2eb72ca8598a9514b9e510151f00550
Zach Stone <zachinjapan888s@gmail.com>
2021-10-19 10:43:35 -0400

    update

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -23,14 +23,16 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {filteredExpenses.length === 0 && <p>No Expenses found. </p>}
+        {filteredExpenses.length > 0 &&
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))}
       </Card>
     </div>
   );
